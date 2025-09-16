f676a023e68046ad0b1ade9591c7538e9e73d8b0
Ricardo Evans <Ricardo.Evans.T0130955@edu.atec.pt>
2025-09-16 16:06:14 +0100

    .

---
+++ JS/Aulas/ExerciciosJS/Ex15.js
@@ -0,0 +1,2 @@
+// Elabore um programa que escreva no ecrã todas as linhas de código ASCII(0 a 255) e o código correspondente. 
+// Dispor de 20 em 20 com a condição de continuação ou saída do programa.
